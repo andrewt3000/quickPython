60ca3a596452f3124855987e9b43643bf66160b0
Andrew Thomas <andrewt@gmail.com>
2019-10-09 02:22:33 -0500

    Update python.py

--- python.py
+++ python.py
@@ -198,7 +198,7 @@ class FileInfo(UserDict):
 
 #self is a keyword to reference class variables and methods. similar to "this" in other languages.  
 
-#the first parameter to a class method is the class. 
+#The first argument of every class method, including init, is always a reference to the current instance of the class. By convention, this argument is always named self. 
 
 # -----------------------------------------------------------------------------
 # -- Dict
